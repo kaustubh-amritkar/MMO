749ee34ee4da7b3ca465ce231d5bb3cfee139046
kaustubh-amritkar <amritkar@wisc.edu>
2025-05-09 16:30:40 -0500

    Initial commit

--- analysis/MOO_phy_with_features.R
+++ analysis/MOO_phy_with_features.R
@@ -12,12 +12,16 @@ library(svglite)
 library(viridis)
 library(dplyr)
 
-phy <- read.tree(file = '/Volumes/bkacar/Kaustubh/MOO/data/foldseek_search/hit_AF_structures_database/TM_score_above_5_structural_comparison/IQ-Tree/MOO_filtered_foldmason_MSA_aa.fa.treefile')
+phy <- read.tree(file = '/Volumes/bkacar/Kaustubh/MOO/data/foldseek_search/hit_AF_structures_database/TM_score_above_5_structural_comparison/IQ-Tree/different_rootings/MOO_filtered_foldmason_MSA_aa.fa.MAD_rooted.treefile')
 
-df = data.frame(read.csv("/Volumes/bkacar/Kaustubh/MOO/data/foldseek_search/hit_AF_structures_database/TM_score_above_5_structural_comparison/filtered_hits_all_info_df.csv"))
+df = data.frame(read.delim("/Volumes/bkacar/Kaustubh/MOO/data/foldseek_search/hit_AF_structures_database/TM_score_above_5_structural_comparison/filtered_hits_all_info_df.tsv"))
 
 annot_df <- df %>% select(target, L2, major_group, superkingdom, phylum, class) %>% rename(label = target)
 
-p <- ggtree(phy, layout = "unrooted") %<+% annot_df + geom_treescale()
+p <- ggtree(phy) %<+% annot_df + geom_treescale()
   
-p + geom_tippoint(aes(color=major_group), size = 2)
+p1 = p + geom_tippoint(aes(color=major_group), size = 3) 
+gheatmap(p1, annot_df[,c("major_group")], offset = 0.5, 
+         width = 0.3,
+         colnames_position = "top",
+         colnames_angle = 90)
