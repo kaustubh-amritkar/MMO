0180e7c650341e10ec937f8cf174abe9e8aed093
kaustubh-amritkar <amritkar@wisc.edu>
2025-07-15 10:07:31 -0500

    Changed all the MOO labels to MMO

--- analysis/MMO_phy_with_features.R
+++ analysis/MMO_phy_with_features.R
@@ -12,7 +12,7 @@ library(svglite)
 library(viridis)
 library(dplyr)
 
-phy <- read.tree(file = '/Volumes/bkacar/Kaustubh/MOO/data/foldseek_search/hit_AF_structures_database/TM_score_above_5_structural_comparison/IQ-Tree/different_rootings/MOO_filtered_foldmason_MSA_aa.fa.MAD_rooted.treefile')
+phy <- read.tree(file = "/Volumes/bkacar/Kaustubh/MOO/data/foldseek_search/hit_AF_structures_database/TM_score_above_5_structural_comparison/IQ-Tree/different_rootings/MOO_filtered_foldmason_MSA_aa.fa.MAD_rooted.treefile")
 
 df = data.frame(read.delim("/Volumes/bkacar/Kaustubh/MOO/data/foldseek_search/hit_AF_structures_database/TM_score_above_5_structural_comparison/filtered_hits_all_info_df.tsv"))
 
@@ -20,7 +20,7 @@ annot_df <- df %>% select(target, L2, major_group, superkingdom, phylum, class) 
 
 p <- ggtree(phy) %<+% annot_df + geom_treescale()
   
-p1 = p + geom_tippoint(aes(color=major_group), size = 3) 
+p1 = p + geom_tippoint(aes(color=superkingdom), size = 3) 
 gheatmap(p1, annot_df[,c("major_group")], offset = 0.5, 
          width = 0.3,
          colnames_position = "top",
